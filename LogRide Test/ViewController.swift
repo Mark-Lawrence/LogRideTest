87dd589f3c4a2bd75d9714e9f23ce07abd7539f5
Mark Lawrence <mark488@ufl.edu>
2018-07-21 17:54:19 -0700

    Initial Commit

---
+++ LogRide Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LogRide Test
+//
+//  Created by Mark Lawrence on 7/21/18.
+//  Copyright © 2018 Mark Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
